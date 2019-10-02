7bee7c2f5c625e0b49c9f08cca62e8726d6816bd
ahshaamabu <ahshaam.m.abu@gmail.com>
2019-10-01 19:38:06 -0500

    test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpSkystone.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpSkystone.java
@@ -8,7 +8,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Telep;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.Servo;
