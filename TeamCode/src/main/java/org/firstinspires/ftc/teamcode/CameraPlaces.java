6c17af60686bd1a8d26d6f01cf42e4a143a040fe
ahshaamabu <ahshaam.m.abu@gmail.com>
2019-11-05 18:54:38 -0600

    f

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CameraPlaces.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+
+public class CameraPlaces {
+    private String Left;
+    private String Middle;
+    private String Right;
+    public static void Camera(){
+
+    }
+    InternalCameraExample Camera = new InternalCameraExample();
+}
