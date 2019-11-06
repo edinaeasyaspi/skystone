e09069fe1ca958451b37b196fca76a44db49b0ec
Wirybluehro <43659174+Wirybluehro@users.noreply.github.com>
2019-11-05 19:56:22 -0600

    Test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arm_comfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arm_comfig.java
@@ -4,42 +4,42 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class Arm_comfig extends All_Knowning_WaterSheep {
 
-    public void NUM2_Lift () {
+    protected void NUM2_Lift () {
         if(AndyMark_motor_elbow.getCurrentPosition()==0){
             AndyMark_motor_elbow.setTargetPosition(560);
             AndyMark_motor_elbow.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             AndyMark_motor_elbow.setPower(0.1);
         }
     }
-    public void NUM3_Lift () {
+    protected void NUM3_Lift () {
         if(AndyMark_motor_elbow.getCurrentPosition()==0){
             AndyMark_motor_elbow.setTargetPosition(560);
             AndyMark_motor_elbow.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             AndyMark_motor_elbow.setPower(0.1);
         }
     }
-    public void NUM4_Lift () {
+    protected void NUM4_Lift () {
         if(AndyMark_motor_elbow.getCurrentPosition()==0){
             AndyMark_motor_elbow.setTargetPosition(560);
             AndyMark_motor_elbow.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             AndyMark_motor_elbow.setPower(0.1);
         }
     }
-    public void NUM5_Lift () {
+    protected void NUM5_Lift () {
         if(AndyMark_motor_elbow.getCurrentPosition()==0){
             AndyMark_motor_elbow.setTargetPosition(560);
             AndyMark_motor_elbow.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             AndyMark_motor_elbow.setPower(0.1);
         }
     }
-    public void NUM6_Lift () {
+    protected void NUM6_Lift () {
         if(AndyMark_motor_elbow.getCurrentPosition()==0){
             AndyMark_motor_elbow.setTargetPosition(560);
             AndyMark_motor_elbow.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             AndyMark_motor_elbow.setPower(0.1);
         }
     }
-    public void NUM7_Lift () {
+    protected void NUM7_Lift () {
         if(AndyMark_motor_elbow.getCurrentPosition()==0){
             AndyMark_motor_elbow.setTargetPosition(560);
             AndyMark_motor_elbow.setMode(DcMotor.RunMode.RUN_TO_POSITION);
