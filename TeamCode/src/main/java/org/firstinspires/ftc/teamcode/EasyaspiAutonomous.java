cba13d3a560a46f8e10390464a7c22ff64ef9785
ahshaamabu <ahshaam.m.abu@gmail.com>
2019-11-07 19:56:25 -0600

    Revert "e"
    This reverts commit 8ba61ccaf802e03be58ee51f98209344138f785e.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EasyaspiAutonomous.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import android.os.Bundle;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+
+@Autonomous
+public class EasyaspiAutonomous extends All_Knowning_WaterSheep {
+    Thread Antitilt = new AntiTiltThread();
+
+
+
+
+
+
+
+
+    @Override
+    public void runOpMode () throws InterruptedException {
+    Antitilt.start();
+
+
+    }
+    public static void Camera(){
+        InternalCameraExample c = new InternalCameraExample();
+        SkystoneLocation location = c.FindSkyStone();
+        if (location == SkystoneLocation.right) {
+            s0 = InternalCameraExample.RED;
+        } else if (InternalCameraExample.location == SkystoneLocation.left) {
+            s2 = InternalCameraExample.RED;
+        } else {
+            s1 = InternalCameraExample.RED;
+        }
+
+        if (InternalCameraExample.RED == s0){
+
+        }
+        else if (InternalCameraExample.RED == s2){
+
+        }
+        else{
+
+        }
+    }
+    InternalCameraExample Camera = new InternalCameraExample();
+}
+
